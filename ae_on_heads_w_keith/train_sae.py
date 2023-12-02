5a9073a39ef01423f4b8aab9995a91355d5025a4
Glen Taggart <glemtaggart@gmail.com>
2023-12-01 20:43:05 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -62,7 +62,7 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
 
 
 def main():
-    ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, l1_coeff=1e-2, nonlinearity=("undying_relu", {"l" : 0.001, "k" : 0.1}))
+    ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, l1_coeff=1e-2, nonlinearity=("undying_relu", {"l" : 0.001, "k" : 1}))
     cfg = z_sae.post_init_cfg(ae_cfg)
     model = z_sae.get_model(cfg)
     all_tokens = z_sae.load_data(model)
