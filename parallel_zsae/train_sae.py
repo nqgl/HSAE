450a6f71083050a0b067053d29132a5794e2d150
Glen Taggart <glemtaggart@gmail.com>
2023-12-02 18:27:13 -0800

    push for remote access

--- parallel_zsae/train_sae.py
+++ parallel_zsae/train_sae.py
@@ -67,7 +67,7 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
 
 def main():
     ae_cfg = z_sae.AutoEncoderConfig(site="z", d_feature=512, 
-                                     l1_coeffs=[1e-3, 2e-3, 15e-4, 8e-4, 6e-4, 3e-3],
+                                     l1_coeffs=[1e-3, 2e-3, 8e-4, 15e-4],
                                      nonlinearity=("undying_relu", {"l" : 0.003, "k" : 0.1}), 
                                      lrs=[3e-4])
     cfg = z_sae.post_init_cfg(ae_cfg)
