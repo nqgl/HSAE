9e6dc5403a968fe27d743aa07b2f51d384ef4ab6
Glen Taggart <glemtaggart@gmail.com>
2023-12-03 14:46:33 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -76,7 +76,7 @@ def main():
 
     ae_cfg = z_sae.AutoEncoderConfig(site="mlp_out", act_size=512, 
                                     l1_coeff=3,
-                                    nonlinearity=("undying_relu", {"l" : 0.003, "k" : 0}), flatten_heads=False,
+                                    nonlinearity=("undying_relu", {"l" : 0, "k" : 0}), flatten_heads=False,
                                     lr=3e-4) #original 3e-4 8e-4 or same but 1e-3 on l1
     # ae_cfg_z = z_sae.AutoEncoderConfig(site="z", act_size=512, 
     #                                  l1_coeff=2e-3,
