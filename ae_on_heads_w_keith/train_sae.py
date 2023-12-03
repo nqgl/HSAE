16663aacb7613696539c3359bb9d7264449246b3
Glen Taggart <glemtaggart@gmail.com>
2023-12-03 15:13:07 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -86,7 +86,7 @@ def main():
     model = z_sae.get_model(cfg)
     all_tokens = z_sae.load_data(model)
     encoder = z_sae.AutoEncoder(cfg)
-    linspace_l1(encoder, 0)
+    # linspace_l1(encoder, 0)
 
     buffer = z_sae.Buffer(cfg, all_tokens, model=model)
     train(encoder, cfg, buffer, model)
