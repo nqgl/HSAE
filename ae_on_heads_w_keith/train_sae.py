b5efb5865adf5fcc8aa384326a34995b5e3577ff
Glen Taggart <glemtaggart@gmail.com>
2023-12-04 15:37:35 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -85,12 +85,14 @@ def linspace_l1(ae, l1_radius):
 # this might not be the case either because l2 is now meaned too
 # so like, l2 /= 512 
 #          l1 /= 
+# so maybe increase lr by 1-2.5 oom?
+    
 def main():
 
     ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, 
                                     l1_coeff=1e-3, dict_mult=8, batch_size=512,
                                     nonlinearity=("relu",{}), flatten_heads=True,
-                                    lr=3e-3) #original 3e-4 8e-4 or same but 1e-3 on l1
+                                    lr=3e-4) #original 3e-4 8e-4 or same but 1e-3 on l1
     # ae_cfg_z = z_sae.AutoEncoderConfig(site="z", act_size=512, 
     #                                  l1_coeff=2e-3,
     #                                  nonlinearity=("undying_relu", {"l" : 0.001, "k" : 0.1}), 
