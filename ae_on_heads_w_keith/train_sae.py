f2e6729ff11fa5c4ff8ceff7311b2a215b807ad6
Glen Taggart <glemtaggart@gmail.com>
2023-12-03 17:47:45 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -32,7 +32,8 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
             loss = encoder.get_loss()
             scaler.scale(loss).backward()
             encoder.make_decoder_weights_and_grad_unit_norm()
-            encoder_optim.step()
+            scaler.step(encoder_optim)
+            scaler.update()
             encoder_optim.zero_grad()
             loss_dict = {"loss": loss.item(), "l2_loss": l2_loss.item(), "l1_loss": l1_loss.sum().item(), "l0_norm": l0_norm.item()}
             del loss, x_reconstruct, l2_loss, l1_loss, acts, l0_norm
