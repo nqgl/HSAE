ccbb071d018fb790eeff3d4fe3406cca6c2402bc
Glen Taggart <glemtaggart@gmail.com>
2023-12-04 23:34:11 -0800

    push for remote access

--- ae_on_heads_w_keith/z_sae.py
+++ ae_on_heads_w_keith/z_sae.py
@@ -186,7 +186,7 @@ class AutoEncoder(nn.Module):
         l0l1_multiplier = self.l0_norm_cached.reshape(-1, 1)
         l0l1_multiplier = torch.max(torch.tensor(1, device="cuda"), l0l1_multiplier - n)
         l0l1_multiplier_sq = (l0l1_multiplier / 2).pow(2)
-        l0l1_special = torch.min(l0l1_multiplier + 1, l0l1_multiplier_sq)
+        l0l1_special = torch.min(l0l1_multiplier, l0l1_multiplier_sq)
         l0l2_multiplier = torch.max(torch.tensor(1, device="cuda"), n - down_flex - self.l0_norm_cached)
         # l0l2_multiplier = 1
         return torch.mean(self.l2_loss_cached * l0l2_multiplier) + torch.sum(torch.mean(l1_coeff * self.l1_loss_cached * l0l1_special, dim=0))
