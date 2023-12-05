129b57cacc000175be959d0f1345a36b787d4148
Glen Taggart <glemtaggart@gmail.com>
2023-12-04 18:28:59 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -94,8 +94,8 @@ def linspace_l1(ae, l1_radius):
 def main():
 
     ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, 
-                                    l1_coeff=1e-3, dict_mult=16, batch_size=1024,
-                                    nonlinearity=("relu",{}), flatten_heads=True, buffer_mult=10000, buffer_refresh_ratio=0.2,
+                                    l1_coeff=8e-4, dict_mult=16, batch_size=1024,
+                                    nonlinearity=("relu",{}), flatten_heads=True, buffer_mult=8000, buffer_refresh_ratio=0.2,
                                     lr=2 ** -12) #original 3e-4 8e-4 or same but 1e-3 on l1
     # ae_cfg_z = z_sae.AutoEncoderConfig(site="z", act_size=512, 
     #                                  l1_coeff=2e-3,
