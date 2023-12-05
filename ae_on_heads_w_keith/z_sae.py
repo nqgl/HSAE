Fix loss calculation in train_sae.py

ae_on_heads_w_keith/z_sae.py
@@ -150,8 +150,8 @@ class AutoEncoder(nn.Module):
         acts = self.nonlinearity(x_cent @ self.W_enc + self.b_enc)
         x_reconstruct = acts @ self.W_dec + self.b_dec
         # self.l2_loss_cached = (x_reconstruct.float() - x.float()).pow(2).mean(-1).mean(0)
-        self.l1_loss_cached = torch.pow(acts.float().abs() + 1e-5, 0.5).mean(dim=(-2)) # TODO fix this embarrasment
-        self.l2_loss_cached = (x_reconstruct.float() - x.float()).abs().mean(-1).mean(0) # don't tell anyone I tried this
+        self.l1_loss_cached = torch.pow(acts.float().abs() + 1e-5, 0.5) # TODO fix this embarrasment
+        self.l2_loss_cached = (x_reconstruct.float() - x.float()).abs().mean(-1) # don't tell anyone I tried this
         if cache_l0:
             self.l0_norm_cached = (acts > 0).float().sum(dim=-1).mean()
         else:
@@ -181,7 +181,9 @@ class AutoEncoder(nn.Module):
         else:
             c_period, c_range = self.cfg.cosine_l1["period"], self.cfg.cosine_l1["range"]
             l1_coeff = self.l1_coeff * (1 + c_range * torch.cos(torch.tensor(2 * torch.pi * self.step_num / c_period).detach()))
-        return self.l2_loss_cached + torch.sum(l1_coeff * self.l1_loss_cached)
+        l1_coeff = l1_coeff.reshape(1, -1)
+        l0_multiplier = self.l0_norm_cached.reshape(-1, 1)
+        return torch.mean(self.l2_loss_cached) + torch.sum(torch.mean(l1_coeff * self.l1_loss_cached * l0_multiplier, dim=0))
 
 
     