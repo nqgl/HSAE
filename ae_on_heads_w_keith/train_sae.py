7b746734d34cde5869451619a054f188fc0e320a
Glen Taggart <glemtaggart@gmail.com>
2023-12-05 17:37:38 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -28,7 +28,7 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
             x_reconstruct = encoder(acts, record_activation_frequency=True)
             loss = encoder.get_loss()
             l2_loss = encoder.l2_loss_cached.mean()
-            l1_loss = encoder.l1_loss_cached
+            l1_loss = encoder.l1_loss_cached.mean()
             l0_norm = encoder.l0_norm_cached.mean() # TODO condisder turning this off if is slows down calculation
             # scaler.scale(loss).backward()
             loss.backward()
