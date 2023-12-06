1a62f36cc2cc2dd55a2a7c037b274af29ca1d9c4
Glen Taggart <glemtaggart@gmail.com>
2023-12-05 20:07:26 -0800

    push for remote access

--- train_sae_on_heads.py
+++ train_sae_on_heads.py
@@ -1,9 +1,9 @@
 import ae_on_heads_w_keith.train_sae as train_sae
-import multiprocessing
+# import multiprocessing
 def main():
     train_sae.main()
 
 if __name__ == "__main__":
-    multiprocessing.set_start_method("spawn")
+    # multiprocessing.set_start_method("spawn"/)
 
     main()
