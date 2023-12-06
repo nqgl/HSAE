4fbcb91cf21960b88c0000f986a363230576c25d
Glen Taggart <glemtaggart@gmail.com>
2023-12-06 00:05:05 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -110,9 +110,9 @@ def main():
     #                             lr=3e-4, cosine_l1={"period": 62063, "range" : 0.05}) #original 3e-4 8e-4 or same but 1e-3 on l1
 
     ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, layer=1,
-                                    l1_coeff=32e-4, dict_mult=32, batch_size=512, beta2=0.99,
-                                    nonlinearity=("relu", {}), flatten_heads=True, buffer_mult=400, buffer_refresh_ratio=0.5,
-                                    lr=3e-4, cosine_l1={"period": 62063, "range" : 0.0}) #original 3e-4 8e-4 or same but 1e-3 on l1
+                                    l1_coeff=32e-4, dict_mult=16, batch_size=1024, beta2=0.99,
+                                    nonlinearity=("relu", {}), flatten_heads=True, buffer_mult=4000, buffer_refresh_ratio=0.5,
+                                    lr=1e-3, cosine_l1={"period": 62063, "range" : 0.0}) #original 3e-4 8e-4 or same but 1e-3 on l1
     # ae_cfg_z = z_sae.AutoEncoderConfig(site="z", act_size=512, 
     #                                  l1_coeff=2e-3,
     #                                  nonlinearity=("undying_relu", {"l" : 0.001, "k" : 0.1}), 
