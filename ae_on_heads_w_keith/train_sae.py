d4da2727bebcb40eda9c28557058fe79391f2b9e
Glen Taggart <glemtaggart@gmail.com>
2023-12-06 16:47:37 -0800

    fix load function

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -110,7 +110,7 @@ def main():
     #                             lr=3e-4, cosine_l1={"period": 62063, "range" : 0.05}) #original 3e-4 8e-4 or same but 1e-3 on l1
 
     ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, layer=1, gram_shmidt_trail=500, num_to_resample = 16,
-                                    l1_coeff=7e-3, dict_mult=32, batch_size=256, beta2=0.99,
+                                    l1_coeff=7e-3, dict_mult=32, batch_size=512, beta2=0.99,
                                     nonlinearity=("relu", {}), flatten_heads=True, buffer_mult=20000, buffer_refresh_ratio=0.4,
                                     lr=3e-5, cosine_l1={"period": 620063, "range" : 0.0125}) #original 3e-4 8e-4 or same but 1e-3 on l1
     # ae_cfg_z = z_sae.AutoEncoderConfig(site="z", act_size=512, 
