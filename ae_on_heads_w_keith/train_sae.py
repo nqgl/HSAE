53c48b2dd1f1c2e7ea0f517e7a741fa0823249ac
Glen Taggart <glemtaggart@gmail.com>
2023-12-07 01:03:41 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -42,7 +42,7 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
             # scaler.update()
             encoder_optim.step()
             encoder_optim.zero_grad()
-            if i % 1000 == 99 and encoder.to_be_reset is not None:
+            if i % 500 == 99 and encoder.to_be_reset is not None:
                 waiting = encoder.to_be_reset.shape[0]
                 wandb.log({"neurons_waiting_to_reset": encoder.to_be_reset.shape[0]})
                 encoder.re_init_neurons(acts.float() - x_reconstruct.float())
