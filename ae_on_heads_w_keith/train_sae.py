a07667977c192021ecf97dfb22c35d00fd604cc4
Glen Taggart <glemtaggart@gmail.com>
2023-12-06 23:22:21 -0800

    push for remote access

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -106,7 +106,7 @@ def linspace_l1(ae, l1_radius):
     # for l1 to get similar gradients, 
     
 ae_cfg = z_sae.AutoEncoderConfig(site="z", act_size=512, layer=1, gram_shmidt_trail = 512, num_to_resample = 64,
-                                l1_coeff=50e-4, dict_mult=32, batch_size=1024, beta2=0.99,
+                                l1_coeff=60e-4, dict_mult=32, batch_size=1024, beta2=0.99,
                                 nonlinearity=("relu", {}), flatten_heads=True, buffer_mult=10000, buffer_refresh_ratio=0.4,
                                 lr=3e-4, cosine_l1={"period": 620063, "range" : 0.0125}) #original 3e-4 8e-4 or same but 1e-3 on l1
 
