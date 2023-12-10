5d74d9b464b2a4e2b17b46b822f6a028576a527f
Glen Taggart <glemtaggart@gmail.com>
2023-12-09 16:04:45 -0800

    fix error

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -105,8 +105,8 @@ def linspace_l1(ae, l1_radius):
 # l1 coeff prevv got multiplied by 128 - 256 but then l2 was like 256 times too
     # for l1 to get similar gradients, 
     
-cfg = z_sae.AutoEncoderConfig(site="mlp_pre", act_size=512, layer=1, gram_shmidt_trail = 512, num_to_resample = 64,
-                                l1_coeff=3e-4, dict_mult=16, batch_size=512, beta2=0.99, data_rescale=1/128,
+cfg = z_sae.AutoEncoderConfig(site="resid_pre", act_size=512, layer=1, gram_shmidt_trail = 512, num_to_resample = 64,
+                                l1_coeff=3e-4, dict_mult=16, batch_size=512, beta2=0.99,
                                 nonlinearity=("relu", {}), flatten_heads=False, buffer_mult=10000, buffer_refresh_ratio=0.4,
                                 lr=3e-3, cosine_l1={"period": 620063, "range" : 0.0125}) #original 3e-4 8e-4 or same but 1e-3 on l1
 
