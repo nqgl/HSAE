403e21efcdbb1c067108c03f2b15b47f44cb1d8e
Glen Taggart <glemtaggart@gmail.com>
2023-12-09 16:14:38 -0800

    fix error

--- ae_on_heads_w_keith/train_sae.py
+++ ae_on_heads_w_keith/train_sae.py
@@ -73,7 +73,7 @@ def train(encoder :z_sae.AutoEncoder, cfg :z_sae.AutoEncoderConfig, buffer :z_sa
                 })
             if i == 13501:
                 encoder.reset_activation_frequencies()    
-            elif i % 15000 == 13501 and i > 1500:
+            elif i % 25000 == 13501 and i > 1500:
                 encoder.save(name=run.name)
                 t1 = time.time()
                 # freqs = get_freqs(model, encoder, buffer, 50, local_encoder=encoder)
@@ -106,7 +106,7 @@ def linspace_l1(ae, l1_radius):
     # for l1 to get similar gradients, 
     
 cfg = z_sae.AutoEncoderConfig(site="resid_post", act_size=512, layer=1, gram_shmidt_trail = 512, num_to_resample = 64,
-                                l1_coeff=6e-3, dict_mult=16, batch_size=512, beta2=0.99,
+                                l1_coeff=2e-2, dict_mult=16, batch_size=512, beta2=0.99,
                                 nonlinearity=("relu", {}), flatten_heads=False, buffer_mult=10000, buffer_refresh_ratio=0.4,
                                 lr=1e-4, cosine_l1={"period": 620063, "range" : 0.0125}) #original 3e-4 8e-4 or same but 1e-3 on l1
 
