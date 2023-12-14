a077d3302eb8a2021b5dfbbc60ac75fae79375be
Glen Taggart <glemtaggart@gmail.com>
2023-12-13 23:23:40 -0800

    push for remote access

--- sae.py
+++ sae.py
@@ -135,7 +135,32 @@ class AutoEncoder(nn.Module):
     
     @torch.no_grad()
     def re_init_neurons(self, x_diff):
-        self.re_init_neurons_gram_shmidt_precise(x_diff)
+        self.re_init_neurons_gram_shmidt_precise_topk(x_diff)
+
+    @torch.no_grad()
+    def re_init_neurons_gram_shmidt_precise_topk(self, x_diff):
+        t = self.cfg.gram_shmidt_trail
+        n_reset = min(x_diff.shape[0], self.cfg.act_size // 2, self.cfg.num_to_resample)
+        v_orth = torch.zeros_like(x_diff)
+        # print(x_diff.shape)
+        # v_orth[0] = F.normalize(x_diff[0], dim=-1)
+        magnitudes = x_diff.norm(dim=-1)
+        indices = torch.topk(magnitudes, n_reset).indices
+        x_diff = x_diff[indices]
+        n_succesfully_reset = n_reset
+        for i in range(n_reset):
+            v_orth[i] = x_diff[i]
+            for j in range(max(0, i - t), i):
+                v_orth[i] -= torch.dot(v_orth[j], v_orth[i]) * v_orth[j] / torch.dot(v_orth[j], v_orth[j])
+            if v_orth[i].norm() < 1e-6:
+                n_succesfully_reset = i
+                break
+            v_orth[i] = F.normalize(v_orth[i], dim=-1)
+            # v_ = x_diff[i] - v_bar * torch.dot(v_bar, x_diff[i])
+            # # print(v_.shape)
+            # v_orth[i] = v_ / v_.norm(dim=-1, keepdim=True)
+        self.reset_neurons(v_orth[:n_succesfully_reset])
+
 
     
     @torch.no_grad()
